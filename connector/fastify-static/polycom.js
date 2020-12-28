e6970a9028eb6bd71b9dbe0a4b57753a2a38921a
gidmoth <gidoramothra@gmail.com>
2020-12-28 18:20:40 +0100

    excessive request logging for check

--- connector/fastify-static/polycom.js
+++ connector/fastify-static/polycom.js
@@ -40,11 +40,6 @@ async function polycomroutes (fastify, options) {
     })
 
     fastify.post('/polycom/:file', async function (req, reply) {
-        console.log(`body: ${JSON.stringify(req.body)}`)
-        console.log(`query: ${JSON.stringify(req.query)}`)
-        console.log(`params: ${JSON.stringify(req.params)}`)
-        console.log(`headers: ${JSON.stringify(req.headers)}`)
-        console.log(`raw: ${req.raw.getHeader('Content-Type')}`)
         return req.params.file
     })
 
