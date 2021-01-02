aa1319a4838063e0bb2b211f8329da816400cbbe
gidmoth <gidoramothra@gmail.com>
2021-01-02 15:33:48 +0100

    checkrequest

--- connector/fastify-static/linphone.js
+++ connector/fastify-static/linphone.js
@@ -13,6 +13,9 @@ async function linphoneroutes(fastify, options) {
     })
 
     fastify.get('/linphone', async function (req, reply) {
+        console.log(req.user)
+        console.log(req)
+        console.log(req.usr)
         let id = req.user.id
         return reply.sendFile(`${id}/linphone.xml`)
     })
