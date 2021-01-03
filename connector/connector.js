3a651530b6bd5eeb33c378662a04b5463af3f064
gidmoth <gidoramothra@gmail.com>
2021-01-03 10:16:53 +0100

    check tls 2

--- connector/connector.js
+++ connector/connector.js
@@ -31,8 +31,7 @@ const fastify = require ('fastify')({
     logger: true,
     https: {
         key: fs.readFileSync(fasticonf.key),
-        cert: fs.readFileSync(fasticonf.cert),
-        secureProtocol: 'TLSv1_2_method'
+        cert: fs.readFileSync(fasticonf.cert)
     }   
 })
 
