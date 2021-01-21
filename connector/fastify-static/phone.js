cefcac04fea545752f00b17bdaa9b01c815304ab
gidmoth <gidoramothra@gmail.com>
2021-01-21 13:23:09 +0100

    change phoneroute

--- connector/fastify-static/phone.js
+++ connector/fastify-static/phone.js
@@ -6,18 +6,17 @@ const provpaths = require('../config').getConfig('provisioningpaths')
 
 async function vcroutes(fastify, options) {
     fastify.register(require('fastify-static'), {
-        root: provpaths.vertocom,
-        prefix: '/phone/'
+        root: provpaths.vertocom
     })
 
-    fastify.get('/phone/config.json', async function (req, reply) {
+    fastify.get('/config.json', async function (req, reply) {
         return { login: req.user.id,
             password: req.user.password,
             name: req.user.name,
             email: req.user.email }
     })
 
-    fastify.get('/phone', async function (req, reply) {
+    fastify.get('/', async function (req, reply) {
         return reply.sendFile('index.html')
     })
 }
