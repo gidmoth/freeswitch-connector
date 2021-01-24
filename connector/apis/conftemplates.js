c5fa445a436659ea0ee893a1d564e3d609a350e4
gidmoth <gidoramothra@gmail.com>
2021-01-24 11:51:06 +0100

    newdirs part 2

--- connector/apis/conftemplates.js
+++ connector/apis/conftemplates.js
@@ -6,6 +6,59 @@
  */
 
 const fastiConf = require('../config').getConfig('fasti');
+const fs = require('fs')
+const path = require('path')
+const sax = require('sax')
+const Provpaths = require('../config').getConfig('provisioningpaths')
+
+const strict = true
+const opts = { trim: true }
+
+const filterCustItems = (user) => {
+  if (fs.existsSync(path.join(Provpaths.polycom, `${user.polymac}/${user.polymac}-directory.xml`))) {
+    let collector = ''
+    let item = 0
+    let saxStream = sax.createStream(strict, opts)
+    saxStream.on('error', function (e) {
+      console.log(e)
+    })
+    saxStream.on('opentag', function (tag) {
+      if (tag.name == 'item') {
+        if (!tag.attributes.hasOwnProperty('server')) {
+          item = 1
+          collector += `<${tag.name}>
+          `}
+      } else {
+        if (item == 1) {
+          collector += `<${tag.name}>`
+        }
+      }
+    })
+    saxStream.on('text', function (txt) {
+      if (item == 1) {
+        collector += `${txt}`
+      }
+    })
+    saxStream.on('closetag', function (tag) {
+      if (tag == 'item' && item == 1) {
+        item = 0
+        collector += `</${tag}>
+                `
+      } else {
+        if (item == 1) {
+          collector += `</${tag}>
+          `}
+      }
+    })
+    saxStream.on('end', function () {
+      return collector
+    })
+    fs.createReadStream(path.join(Provpaths.polycom, `${user.polymac}/${user.polymac}-directory.xml`))
+      .pipe(saxStream)
+  } else {
+    return ''
+  }
+}
 
 const getConfFile = (conf) => {
   let xml = ''
@@ -42,10 +95,11 @@ const getConfFile = (conf) => {
   return xml
 }
 
-getPolyDir = (confs) => {
+getPolyDir = (confs, user) => {
   let dirxml = `<directory>
     <item_list>
 `
+  dirxml += filterCustItems(user)
   for (let conf of confs) {
     dirxml += `        <item server='yes'>
             <fn>${conf.name}</fn>
