599ad64aef5501c4a59b0238ce61adffe9de026d
root <root@gsphone.c8h10n4o2.gs>
2021-04-26 10:43:45 +0000

    websocket keepalive and login done

--- connector/fastify-socket/livePoint.js
+++ connector/fastify-socket/livePoint.js
@@ -2,10 +2,23 @@
  * Websocket connection to propagate liveState
  */
 
+
+const fastiConf = require('../config').getConfig('fasti');
+
 function noop() { }
 
 function heartbeat() {
     this.isAlive = true;
+}
+
+function checkCreds(cstring, arr) {
+    let user = arr.filter(usr => { return usr.name === `${cstring.split(':')[0]}` })[0]
+    if (user !== undefined
+        && user.password === `${cstring.split(':')[1]}`
+        && user.context === fastiConf.apiallow) { 
+        return true
+    }
+    return false
 }
 
 async function liveroutes(fastify, options) {
@@ -16,22 +29,34 @@ async function liveroutes(fastify, options) {
         }
     })
 
-    fastify.addHook('onRequest', (req, repl, done) => {
-        console.log(req.headers)
+    fastify.addHook('onRequest', (conn, repl, done) => {
+        if (conn.query.login === undefined) {
+            console.log('got wrong query!')
+            conn.socket.write('HTTP/1.1 401 Unauthorized\r\n\r\n')
+            conn.socket.destroy()
+            return repl.send('Unauthorized')
+        }
+        if (! checkCreds(conn.query.login, fastify.xmlState.users)) {
+            console.log('got wrong creds!')
+            conn.socket.write('HTTP/1.1 401 Unauthorized\r\n\r\n')
+            conn.socket.destroy()
+            return repl.send('Unauthorized')
+        }
+        console.log('yay, login!')
         done()
     })
 
     fastify.get('/api/live', { websocket: true }, (conn, req) => {
 
 
-//        console.log(JSON.stringify(req.query))
+        //        console.log(JSON.stringify(req.query))
 
         conn.socket.on('open', heartbeat)
 
         conn.socket.on('pong', heartbeat)
-    
+
         const interval = setInterval(() => {
-//            console.log(`my clients: ${fastify.websocketServer.clients.size}`)
+            //            console.log(`my clients: ${fastify.websocketServer.clients.size}`)
             fastify.websocketServer.clients.forEach(sock => {
                 if (sock.isAlive === false) {
                     console.log('destroying unresponsive client')
@@ -41,7 +66,7 @@ async function liveroutes(fastify, options) {
                 sock.ping(noop)
             })
         }, 30000)
-    
+
         fastify.websocketServer.on('close', () => {
             clearInterval(interval)
         })
