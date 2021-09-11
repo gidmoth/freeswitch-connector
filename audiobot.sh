769133ac631cc1b54953aba79434d93715c094e1
Stefan Gross <gidoramothra@gmail.com>
2021-09-11 13:29:59 +0200

    audiobot delete old files

--- audiobot.sh
+++ audiobot.sh
@@ -7,4 +7,5 @@ inotifywait -e close_write $1/$2 | \
       export mname=$(echo $2|sed 's/.wav$/.mp3/')
       ffmpeg -i  $1/$2 -y -af dynaudnorm -ab 64k $1/$mname
       rm $1/$2
+      find $1/* -mtime 7 -delete
     done
